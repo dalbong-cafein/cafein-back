c5f4126f191c6356cf00c8cb4ad2dd00de84a344
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-12 17:07:33 +0900

    init

---
+++ src/main/java/com/dalbong/cafein/CafeinApplication.java
@@ -0,0 +1,13 @@
+package com.dalbong.cafein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalbong/cafein/CafeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalbong.cafein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
