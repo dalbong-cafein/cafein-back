0e33ddbbdc84086abd5b4d0ba17cb197df54efe0
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-18 04:48:21 +0900

    BusinessHours 엔티티 생성

---
+++ src/main/java/com/dalbong/cafein/domain/businessHours/BusinessHours.java
@@ -0,0 +1,74 @@
+package com.dalbong.cafein.domain.businessHours;
+
+import com.dalbong.cafein.domain.store.Store;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class BusinessHours {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long businessHoursId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="open", column = @Column(name="mon_open")),
+            @AttributeOverride(name="closed", column = @Column(name="mon_closed"))
+    })
+    private Day onMon;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="open", column = @Column(name="tue_open")),
+            @AttributeOverride(name="closed", column = @Column(name="tue_closed"))
+    })
+    private Day onTue;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="open", column = @Column(name="wed_open")),
+            @AttributeOverride(name="closed", column = @Column(name="wed_closed"))
+    })
+    private Day onWed;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="open", column = @Column(name="thu_open")),
+            @AttributeOverride(name="closed", column = @Column(name="thu_closed"))
+    })
+    private Day onThu;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="open", column = @Column(name="fri_open")),
+            @AttributeOverride(name="closed", column = @Column(name="fri_closed"))
+    })
+    private Day onFri;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="open", column = @Column(name="sat_open")),
+            @AttributeOverride(name="closed", column = @Column(name="sat_closed"))
+    })
+    private Day onSat;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="open", column = @Column(name="sun_open")),
+            @AttributeOverride(name="closed", column = @Column(name="sun_closed"))
+    })
+    private Day onSun;
+}
--- src/main/java/com/dalbong/cafein/domain/store/Store.java
+++ src/main/java/com/dalbong/cafein/domain/store/Store.java
@@ -1,5 +1,6 @@
 package com.dalbong.cafein.domain.store;
 
+import com.dalbong.cafein.domain.BaseEntity;
 import com.dalbong.cafein.domain.address.Address;
 import com.dalbong.cafein.domain.member.Member;
 import lombok.*;
@@ -12,7 +13,7 @@ import javax.persistence.*;
 @Getter
 @ToString
 @Entity
-public class Store {
+public class Store extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long storeId;
@@ -21,10 +22,22 @@ public class Store {
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
+    @Column(nullable = false)
     private String storeName;
+
+    private int americano;
 
     @Embedded
     private Address address;
 
 
+    private String website;
+
+    private String phone;
+
+    @Column(nullable = false)
+    private String mapX;
+
+    @Column(nullable = false)
+    private String mapY;
 }
