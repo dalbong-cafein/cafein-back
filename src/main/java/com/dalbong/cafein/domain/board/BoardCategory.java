534bfe836c19abaaa50c88eb73a82ceabf455587
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-24 17:08:52 +0900

    feature : BoardCategory 도메인 생성

---
+++ src/main/java/com/dalbong/cafein/domain/board/BoardCategory.java
@@ -0,0 +1,21 @@
+package com.dalbong.cafein.domain.board;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Entity
+public class BoardCategory {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(nullable = false)
+    private String categoryName;
+    
+}
