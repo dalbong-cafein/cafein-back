3d703d146e1381926ee0c2d57b00f9e47f436864
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-24 17:37:12 +0900

    feature: 게시글 등록 기능 완료

---
+++ src/main/java/com/dalbong/cafein/service/BoardServiceImpl.java
@@ -0,0 +1,30 @@
+package com.dalbong.cafein.service;
+
+import com.dalbong.cafein.domain.board.Board;
+import com.dalbong.cafein.domain.board.BoardRepository;
+import com.dalbong.cafein.dto.board.BoardResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    private final BoardRepository boardRepository;
+
+    /**
+     * 게시글 등록
+     */
+    @Transactional
+    @Override
+    public Board register(BoardResDto boardResDto, Long principalId) {
+
+        Board board = boardResDto.toEntity(principalId);
+
+        return boardRepository.save(board);
+    }
+
+
+}
