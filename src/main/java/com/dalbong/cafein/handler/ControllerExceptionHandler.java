d73c90597ba9ffb7584531e72a37dc6ea55dd265
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-28 18:28:12 +0900

    exception handler 추가

---
+++ src/main/java/com/dalbong/cafein/handler/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.dalbong.cafein.handler;
+
+import com.dalbong.cafein.dto.CMRespDto;
+import com.dalbong.cafein.handler.exception.CustomException;
+import com.dalbong.cafein.handler.exception.CustomValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    //유효성 검사
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<CMRespDto<?>> validationApiException(CustomValidationException e){
+
+        return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),e.errorMap), HttpStatus.BAD_REQUEST);
+    }
+
+    //에러 처리
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<CMRespDto<?>> apiException(CustomException e){
+
+        return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),null), HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/com/dalbong/cafein/handler/exception/CustomValidationException.java
@@ -0,0 +1,23 @@
+package com.dalbong.cafein.handler.exception;
+
+import java.util.Map;
+
+public class CustomValidationException extends RuntimeException{
+
+    private static final long serialVersionUID=1L;
+
+    public Map<String,String> errorMap;
+
+    public CustomValidationException(String message){
+        super(message);
+    }
+
+    public CustomValidationException(String message, Map<String,String> errorMap) {
+        super(message);
+        this.errorMap = errorMap;
+    }
+
+    public Map<String,String> getErrorMap(){
+        return this.errorMap;
+    }
+}
