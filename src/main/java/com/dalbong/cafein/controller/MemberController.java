333189605e12f316077468c8c43f03171fef0282
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-03-02 19:41:20 +0900

    feature: 휴대폰 번호 수정 api 생성

---
+++ src/main/java/com/dalbong/cafein/controller/MemberController.java
@@ -0,0 +1,31 @@
+package com.dalbong.cafein.controller;
+
+import com.dalbong.cafein.config.auth.PrincipalDetails;
+import com.dalbong.cafein.dto.CMRespDto;
+import com.dalbong.cafein.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 휴대폰 번호 수정
+     */
+    @PatchMapping("/members/{memberId}/phone")
+    public ResponseEntity<?> modifyPhone(@RequestBody String phone,
+                                         @AuthenticationPrincipal PrincipalDetails principalDetails){
+
+        memberService.modifyPhone(phone, principalDetails.getMember().getMemberId());
+
+        return new ResponseEntity<>(new CMRespDto<>(1, "휴대폰 번호 수정 성공",null), HttpStatus.OK);
+    }
+}
