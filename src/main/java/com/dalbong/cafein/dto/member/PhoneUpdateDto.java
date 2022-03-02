9cf0230007d0f0534d86c0367966dc2d3048e54f
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-03-02 20:06:08 +0900

    phoneUpdateDto 생성

---
+++ src/main/java/com/dalbong/cafein/dto/member/PhoneUpdateDto.java
@@ -0,0 +1,13 @@
+package com.dalbong.cafein.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PhoneUpdateDto {
+
+    private String phone;
+}
