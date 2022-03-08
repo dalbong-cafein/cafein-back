08e0652cbd0f64832b204ce06a0df71f89e47efc
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-03-08 17:07:07 +0900

    feature: ReviewImage 생성

---
+++ src/main/java/com/dalbong/cafein/domain/image/ReviewImage.java
@@ -0,0 +1,24 @@
+package com.dalbong.cafein.domain.image;
+
+import com.dalbong.cafein.domain.review.Review;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@ToString(exclude = {"review"})
+@DiscriminatorValue("review")
+@Entity
+public class ReviewImage extends Image{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    protected ReviewImage(){}
+
+    public ReviewImage(Review review, String imageUrl){
+        this.review = review;
+    }
+}
