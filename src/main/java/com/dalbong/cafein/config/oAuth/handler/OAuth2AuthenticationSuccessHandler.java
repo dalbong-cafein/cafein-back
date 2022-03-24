bb87e002d9e9f2dad040f6d9b5eb209ad4939ef1
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-03-25 01:06:06 +0900

    배포

--- src/main/java/com/dalbong/cafein/config/oAuth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/dalbong/cafein/config/oAuth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -56,6 +56,7 @@ public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthent
 
         }
 
+        
         //redirect
         Cookie findCookie = cookieUtil.getCookie(request, "os");
 
