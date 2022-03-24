d33c4b38c7ba3a317cf0b14cb000370d5fa2c10a
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-03-25 00:32:36 +0900

    "/favicon.ico" 해결

--- src/main/java/com/dalbong/cafein/config/SecurityConfig.java
+++ src/main/java/com/dalbong/cafein/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,6 +34,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder encode() {
         return new BCryptPasswordEncoder();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**",
+                        "/favicon.ico"
+                );
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
