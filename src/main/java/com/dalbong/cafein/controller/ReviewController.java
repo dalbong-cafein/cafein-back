6dcb807c5eb730e9c1903c52492dca053aa30c74
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-03-31 02:51:02 +0900

    feature : 1. 리뷰 수정 api 2. 리뷰 삭제 api

--- src/main/java/com/dalbong/cafein/controller/ReviewController.java
+++ src/main/java/com/dalbong/cafein/controller/ReviewController.java
@@ -3,14 +3,15 @@ package com.dalbong.cafein.controller;
 import com.dalbong.cafein.config.auth.PrincipalDetails;
 import com.dalbong.cafein.dto.CMRespDto;
 import com.dalbong.cafein.dto.review.ReviewRegDto;
+import com.dalbong.cafein.dto.review.ReviewUpdateDto;
 import com.dalbong.cafein.service.review.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -24,12 +25,34 @@ public class ReviewController {
      * 리뷰 등록
      */
     @PostMapping("/reviews")
-    public ResponseEntity<?> register(@Validated ReviewRegDto reviewRegDto,
+    public ResponseEntity<?> register(@Validated ReviewRegDto reviewRegDto, BindingResult bindingResult,
                                       @AuthenticationPrincipal PrincipalDetails principalDetails) throws IOException {
 
         reviewService.register(reviewRegDto, principalDetails.getMember().getMemberId());
 
         return new ResponseEntity<>(new CMRespDto<>(1,"리뷰 등록 성공",null), HttpStatus.CREATED);
+    }
 
+    /**
+     * 리뷰 수정
+     */
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<?> modify(@Validated ReviewUpdateDto reviewUpdateDto, BindingResult bindingResult) throws IOException {
+
+        reviewService.modify(reviewUpdateDto);
+
+        return new ResponseEntity<>(new CMRespDto<>(1, "리뷰 수정 성공", null), HttpStatus.OK);
+    }
+
+
+    /**
+     * 리뷰 삭제
+     */
+    @DeleteMapping("reviews/{reviewId}")
+    public ResponseEntity<?> remove(@PathVariable("reviewId") Long reviewId){
+
+        reviewService.remove(reviewId);
+
+        return new ResponseEntity<>(new CMRespDto<>(1, "리뷰 삭제 성공",null), HttpStatus.OK);
     }
 }
