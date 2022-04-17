bf2b71bc10f901b42feea02a628656d312949e3b
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-04-17 23:46:51 +0900

    feature : 관리지단 리뷰 리스트 조회 api

---
+++ src/main/java/com/dalbong/cafein/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.dalbong.cafein.controller;
+
+import com.dalbong.cafein.dto.CMRespDto;
+import com.dalbong.cafein.dto.admin.review.AdminReviewListDto;
+import com.dalbong.cafein.dto.page.PageRequestDto;
+import com.dalbong.cafein.dto.review.ReviewListDto;
+import com.dalbong.cafein.service.review.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class AdminController {
+
+    private final ReviewService reviewService;
+
+    /**
+     * 전체 리뷰 리스트 조회
+     */
+    @GetMapping("/admin/reviews")
+    public ResponseEntity<?> getReviewListOfStore(PageRequestDto requestDto){
+
+        AdminReviewListDto adminReviewListDto = reviewService.getReviewListOfAdmin(requestDto);
+
+        return new ResponseEntity<>(new CMRespDto<>(1, "관리자단 리뷰 리스트 조회 성공", adminReviewListDto), HttpStatus.OK);
+    }
+
+}
