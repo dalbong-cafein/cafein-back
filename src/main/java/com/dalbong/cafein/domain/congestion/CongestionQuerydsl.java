7f3b63593c4cd0a17a7ce2c6910cc15cee58deb7
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-04-27 18:10:48 +0900

    refactor : 혼잡도 등록 기능 - 3시간이내 체크

--- src/main/java/com/dalbong/cafein/domain/congestion/CongestionQuerydsl.java
+++ src/main/java/com/dalbong/cafein/domain/congestion/CongestionQuerydsl.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 public interface CongestionQuerydsl {
 
     /**
-     * 혼잡도 2시간이내 등록 여부
+     * 혼잡도 3시간이내 등록 여부
      */
     boolean existWithinTime(Long storeId, Long principalId);
 
