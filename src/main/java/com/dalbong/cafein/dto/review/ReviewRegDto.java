2f18c94446e7401bb4e0ebdf32a69fbaa37716aa
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-05-15 02:22:12 +0900

    refactor : ReviewRegDto - 1. 유효성 검사 2. imageFiles 디폴트 값 설정

--- src/main/java/com/dalbong/cafein/dto/review/ReviewRegDto.java
+++ src/main/java/com/dalbong/cafein/dto/review/ReviewRegDto.java
@@ -12,6 +12,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.ArrayList;
 import java.util.List;
 
 @Builder
@@ -24,18 +27,22 @@ public class ReviewRegDto {
 
     private Recommendation recommendation;
 
+    @Max(value = 4) @Min(value = 1)
     private int socket;
 
+    @Max(value = 4) @Min(value = 1)
     private int wifi;
 
+    @Max(value = 4) @Min(value = 1)
     private int restroom;
 
+    @Max(value = 4) @Min(value = 1)
     private int tableSize;
 
     @Length(max = 100)
     private String content;
 
-    private List<MultipartFile> imageFiles;
+    private List<MultipartFile> imageFiles = new ArrayList<>();
 
     //TODO member 엔티티와 양방향 연관관계
     public Review toEntity(Long principalId, Store store){
