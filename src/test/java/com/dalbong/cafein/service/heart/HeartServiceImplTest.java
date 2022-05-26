8da0fbc69bf467fcb9dc44f5980d4d18d271928c
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-05-27 03:30:19 +0900

    eb 배포

--- src/test/java/com/dalbong/cafein/service/heart/HeartServiceImplTest.java
+++ src/test/java/com/dalbong/cafein/service/heart/HeartServiceImplTest.java
@@ -14,6 +14,7 @@ import com.dalbong.cafein.domain.store.StoreRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
