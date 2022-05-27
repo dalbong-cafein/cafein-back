1c664f0976c685fe33032456cc804fa253c928fd
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-05-28 03:57:25 +0900

    feature : 관리자단 회원별 신고내역 조회 api

--- src/main/java/com/dalbong/cafein/service/report/ReportServiceImpl.java
+++ src/main/java/com/dalbong/cafein/service/report/ReportServiceImpl.java
@@ -2,10 +2,15 @@ package com.dalbong.cafein.service.report;
 
 import com.dalbong.cafein.domain.report.Report;
 import com.dalbong.cafein.domain.report.ReportRepository;
+import com.dalbong.cafein.dto.admin.report.AdminReportListResDto;
+import com.dalbong.cafein.dto.admin.report.AdminReportResDto;
 import com.dalbong.cafein.dto.report.ReportRegDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional
 @RequiredArgsConstructor
@@ -24,5 +29,20 @@ public class ReportServiceImpl implements ReportService{
         Report report = reportRegDto.toEntity();
 
         return reportRepository.save(report);
+    }
+
+    /**
+     * 관리자단 회원별 신고 내역 조회
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public AdminReportListResDto getReportListOfAdmin(Long memberId) {
+
+        List<Report> reportList = reportRepository.getReportListByMemberId(memberId);
+
+        List<AdminReportResDto> adminReportResDtoList =
+                reportList.stream().map(report -> new AdminReportResDto(report)).collect(Collectors.toList());
+
+        return new AdminReportListResDto(reportList.size(), adminReportResDtoList);
     }
 }
