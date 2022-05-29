8356be6ab281a9eb39cc6ce20e5b9d3ce7629eba
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-05-29 23:48:03 +0900

    feature : 회원별 금일 받은 스티커 개수 조회 쿼리

--- src/main/java/com/dalbong/cafein/domain/sticker/StickerRepository.java
+++ src/main/java/com/dalbong/cafein/domain/sticker/StickerRepository.java
@@ -1,8 +1,7 @@
 package com.dalbong.cafein.domain.sticker;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StickerRepository extends JpaRepository<Sticker, Long>, StickerRepositoryQuerydsl {
-
-
 }
