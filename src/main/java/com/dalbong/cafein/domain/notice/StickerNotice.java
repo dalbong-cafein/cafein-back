7c95e4121c275787d862c950939fa25f6af9682b
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-06-01 18:04:30 +0900

    feature : Notice Entity 설계

---
+++ src/main/java/com/dalbong/cafein/domain/notice/StickerNotice.java
@@ -0,0 +1,32 @@
+package com.dalbong.cafein.domain.notice;
+
+import com.dalbong.cafein.domain.member.Member;
+import com.dalbong.cafein.domain.sticker.Sticker;
+import com.dalbong.cafein.domain.store.Store;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"sticker"})
+@DiscriminatorValue("sticker")
+@Entity
+public class StickerNotice extends Notice{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sticker_id")
+    private Sticker sticker;
+
+    public StickerNotice(Sticker sticker, Member toMember){
+        super(toMember,"스티커 증정 완료");
+        this.sticker = sticker;
+    }
+
+    public StickerNotice(Sticker sticker, Member toMember, String content){
+        super(toMember, content);
+        this.sticker = sticker;
+    }
+}
