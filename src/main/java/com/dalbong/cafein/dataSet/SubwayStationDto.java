97d0606555eba658f6fa251c72242f692660ab42
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-08-04 01:34:30 +0900

    feat : SubwayStationDto 생성

---
+++ src/main/java/com/dalbong/cafein/dataSet/SubwayStationDto.java
@@ -0,0 +1,15 @@
+package com.dalbong.cafein.dataSet;
+
+import lombok.Data;
+
+@Data
+public class SubwayStationDto {
+
+    private String statn_nm;
+
+    private String route;
+
+    private Double crdnt_x;
+
+    private Double crdnt_y;
+}
