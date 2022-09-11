55b09e26391dfa664ea7fc3556897e98fad33804
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-09-12 02:13:32 +0900

    feat : 카페 등록 시 - StoreImage의 isGoogle 필드값 false로 기본값 지정

--- src/main/java/com/dalbong/cafein/dataSet/store/google/GoogleSearchService.java
+++ src/main/java/com/dalbong/cafein/dataSet/store/google/GoogleSearchService.java
@@ -237,7 +237,7 @@ public class GoogleSearchService {
 
                 String imageUrl = s3Upload(store, response);
 
-                storeImageRepository.save(new StoreImage(store,imageUrl));
+                storeImageRepository.save(new StoreImage(store,imageUrl, true));
 
                 i++;
                 if (i == 5) break;
