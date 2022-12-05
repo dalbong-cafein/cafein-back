99286be00c8d739e40a527dc7832619f46813e83
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-12-05 20:20:54 +0900

    feat : University Entity 생성

---
+++ src/main/java/com/dalbong/cafein/domain/university/UniversityRepository.java
@@ -0,0 +1,6 @@
+package com.dalbong.cafein.domain.university;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityRepository extends JpaRepository<University,Long> {
+}
