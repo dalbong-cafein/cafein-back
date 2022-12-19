caf236d2d33f9786b00d0df30140d69619f15360
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-12-19 18:11:11 +0900

    refactor : SqlFunctionUtil - private 함수 -> public 변경

--- src/main/java/com/dalbong/cafein/util/SqlFunctionUtil.java
+++ src/main/java/com/dalbong/cafein/util/SqlFunctionUtil.java
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class SqlFunctionUtil {
 
-    private static NumberTemplate<Double> acos(Object num) {
+    public static NumberTemplate<Double> acos(Object num) {
         return Expressions.numberTemplate(Double.class, "function('acos',{0})", num);
     }
 
-    private static NumberTemplate<Double> sin(Object num) {
+    public static NumberTemplate<Double> sin(Object num) {
         return Expressions.numberTemplate(Double.class, "function('sin',{0})", num);
     }
 
-    private static NumberTemplate<Double> cos(Object num) {
+    public static NumberTemplate<Double> cos(Object num) {
         return Expressions.numberTemplate(Double.class, "function('cos',{0})", num);
     }
 
-    private static NumberTemplate<Double> radians(Object num) {
+    public static NumberTemplate<Double> radians(Object num) {
         return Expressions.numberTemplate(Double.class, "function('radians',{0})", num);
     }
 
